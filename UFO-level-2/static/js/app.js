90f01d43a54c25522ba619ff9b26507c2957c16a
Matt Garcia <48602938+Mattyapolis@users.noreply.github.com>
2019-09-17 17:28:11 -0500

    Add files via upload

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -29,17 +29,40 @@ button.on('click', function() {
     //reference form input value
     var dateElement = d3.select('#datetime');
     var dateInput  = dateElement.property('value');
+    var cityElement = d3.select('#city');
+    var cityInput = cityElement.property('value');
+    var stateElement = d3.select('#state');
+    var stateInput = stateElement.property('value');
+    var countryElement = d3.select('#country');
+    var countryInput = countryElement.property('value');
+    var shapeElement = d3.select('#shape');
+    var shapeInput = shapeElement.property('value');
+    var durationElement = d3.select('#durationMinutes');
+    var durationInput = durationElement.property('value');
     
     console.log(dateInput)//check date input capture
 
     //create filter to return ojbects with 'datetime' key value that matches user input
-    var filteredData = tableData.filter(ufoRecord => ufoRecord.datetime === dateInput);
-    
-    console.log(filteredData)//check filtered data object
+    var dateFilter = tableData.filter(ufoRecord => ufoRecord.datetime === dateInput);
+    var cityFilter = tableData.filter(ufoRecord => ufoRecord.city === cityInput);
+    var stateFilter = tableData.filter(ufoRecord => ufoRecord.state === stateInput);
+    var countryFilter = tableData.filter(ufoRecord => ufoRecord.country === countryInput);
+    var shapeFilter = tableData.filter(ufoRecord => ufoRecord.shape === shapeInput);
+    var durationFilter = tableData.filter(ufoRecord => ufoRecord.durationMinutes == durationInput);
+    // console.log(filteredData)//check filtered data object
+
+
+
+
 
     //check that date is entered in valid format and if so populate table with filtered data
-    if (moment(dateInput, 'M/D/YYYY', true).isValid()){
-        createTable(filteredData);
+    if (moment(dateInput, 'M/D/YYYY', true).isValid() || dateInput.length==0){
+        createTable(dateFilter)
+        createTable(cityFilter)
+        createTable(stateFilter)
+        createTable(countryFilter)
+        createTable(shapeFilter)
+        createTable(durationFilter)
     }
     else {
         alert("Please enter a date in January 2010 as M/D/YYYY format");
